bae174f3b4febaa50bf6263af2760e07b445398a
AhmadAbuyahya <Ahmadabuyahya998@gmail.com>
2021-02-09 17:35:05 +0200

    lab03

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 'use strict';
+let score=0;
 let userName = prompt('Hello! What\'s your name?');
 alert('Hello '+userName+', welcome to my website! are you ready to play a little guessing game about me? ');
 let answer1 = prompt('Is my name Ahmad? (y/n)');
 if (answer1.toLocaleLowerCase()==='y'){
   alert('That\'s correct!');
+  score++;
 }
 else {
   alert('That\'s incorrect!');
@@ -11,6 +13,7 @@ else {
 let answer2 = prompt('Am I Jordanian? (y/n)');
 if (answer2.toLocaleLowerCase()==='y'){
   alert('That\'s correct!');
+  score++;
 }
 else {
   alert('That\'s incorrect!');
@@ -18,6 +21,7 @@ else {
 let answer3 = prompt('Am I a student at ASAC? (y/n)');
 if (answer3.toLocaleLowerCase()==='y'){
   alert('That\'s correct!');
+  score++;
 }
 else {
   alert('That\'s incorrect!');
@@ -28,13 +32,55 @@ if (answer4.toLocaleLowerCase()==='y'){
 }
 else {
   alert('That\'s correct!');
+  score++;
 }
 let answer5 = prompt('Do I enjoy learning coding at ASAC? (y/n)');
 if (answer5.toLocaleLowerCase()==='y'){
   alert('That\'s correct!');
+  score++;
 }
 else {
   alert('That\'s incorrect!');
 }
-alert('Thank you '+userName+' for playing my game! you can learn more about me in the next page.');
+
+let counter = 0;
+let answerWrong = false;
+const myAge=23;
+while (counter < 4) {
+  let ageGuess= prompt('Can you guess my age?');
+  if(ageGuess>myAge){
+    alert('That\'s too high!');
+  }
+  else if (ageGuess<myAge){
+    alert('That\'s too low!');
+  }
+  else if (ageGuess==myAge){
+    alert('You got it! good job.');
+    answerWrong = true;
+    score++;
+    break;
+  }
+  counter++;
+}
+if (!answerWrong) {
+  alert('Sorry, you ran out of guesses, my age is 23.');
+}
+
+let favFood=['mansaf','pizza','spaghetti','burger','falafel','kebab'];
+for(let i=0 ; i < 6 ; i++ ){
+  let foodGuess=prompt('Can you guess my favorite food?');
+  for(let j=0; j<favFood.length; j++){
+    if (foodGuess === favFood[j]){
+      alert('You got it! good job.');
+      score++;
+      i=6;
+      break;
+    }
+  }
+  if (i!==6){
+    alert('Wrong answer, try again!');
+  }
+}
+alert('My favorite foods are: mansaf, pizza, spaghetti, burger and kebab');
+alert('Thank you '+userName+' for playing my game! you answered '+score+'/7 questions correctly, you can learn more about me in the next page.');
 
